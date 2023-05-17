a8c5dac6519345ecbecedba62686a8bac72b8685
윤다빈 <116141225+czmcm5@users.noreply.github.com>
2023-05-18 02:37:43 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -2,6 +2,7 @@ class Solution {
     public int solution(int n, int k) {
         int answer = (12000 * n) + (2000 * k);
         
+        // 근데 이럴거면 굳이 if쓸필요가 없엇네.. ㅋ 어짜피 몫구하면 0이니깜.. 
         if (n >= 10){
             answer = answer - 2000 * (n/10);
         }
