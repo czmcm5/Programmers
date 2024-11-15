3ff5b359f7f1886ad861bce6e9c8ccd06a6bf2ab
윤다빈 <116141225+czmcm5@users.noreply.github.com>
2024-11-15 23:40:08 +0900

    [level 0] Title: 배열 만들기 4, Time: 29.15 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    var stk = [];
+  let i = 0;
+  while (i < arr.length) {
+    if (!stk.length || (stk.length && stk[stk.length - 1] < arr[i])) {
+      stk.push(arr[i]);
+      i++;
+    } else {
+      stk.pop();
+    }
+  }
+  return stk;
+}
