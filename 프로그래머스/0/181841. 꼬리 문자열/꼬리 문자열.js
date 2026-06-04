49861ee29914d18751249fd95481354174bb685d
윤다빈 <116141225+czmcm5@users.noreply.github.com>
2026-06-05 00:49:02 +0900

    [JS] lv.0 꼬리 문자열 정답 수정

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,9 +1,14 @@
-function solution(str_list, ex) {
-  var answer = "";
+/*
+이전 코드의 피드백: .map()을 사용하는 것은 해당 메서드의 용도와 맞지 않다. 누군가가 코드를 읽을 때 의도를 쉽게 알 수 없다.
+개선 사항: 배열을 순회하는 용도의 메서드 .forEach()를 사용하거나 배열의 요소를 누산하는 .reduce를 사용하기
+*/
 
-  str_list.map((item) => {
-    if (!item.includes(ex)) answer += item;
-  });
+
+function solution(str_list, ex) {
+  const answer = str_list.reduce(
+    (acc, item) => (item.includes(ex) ? acc : acc + item),
+    "",
+  );
 
   return answer;
 }
